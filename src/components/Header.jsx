c368dc1ceb6afad867f82885ba88b15fee502b12
B <B@Aravinths-MacBook-Pro.local>
2023-10-31 20:49:28 +0000

    hide other end points

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,8 +9,8 @@ export default function Header() {
           <a class="" href="https://brightlystake.com">Brightlystake</a>
         </li>
         <li className='li-r'><a class="active" href="/rpc-status">RPC</a></li>
-        <li className='li-r'><a class="active" href="/grpc-status">gRPC</a></li>
-        <li className='li-r'><a class="active" href="/latency">Latency</a></li>
+        {/* <li className='li-r'><a class="active" href="/grpc-status">gRPC</a></li>
+        <li className='li-r'><a class="active" href="/latency">Latency</a></li> */}
       </ul>
     </div>
   )
